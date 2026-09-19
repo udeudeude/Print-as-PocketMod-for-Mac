35d7581c3ef5d99216601e56e1cf0c2234bb1a88
udeudeude <udeudeude@gmail.com>
2026-09-18 20:19:14 -0400

    Add direct PocketMod command-line converter

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(name: "PocketModCore", targets: ["PocketModCore"]),
         .executable(name: "PocketModApp", targets: ["PocketModApp"]),
+        .executable(name: "PocketModCLI", targets: ["PocketModCLI"]),
     ],
     targets: [
         .target(
@@ -19,6 +20,14 @@ let package = Package(
         ),
         .executableTarget(
             name: "PocketModApp",
+            dependencies: ["PocketModCore"],
+            linkerSettings: [
+                .linkedFramework("PDFKit"),
+                .linkedFramework("AppKit"),
+            ]
+        ),
+        .executableTarget(
+            name: "PocketModCLI",
             dependencies: ["PocketModCore"],
             linkerSettings: [
                 .linkedFramework("PDFKit"),
